73b2048f9493cee786d338305704cf8eb1a8b68d
Thiago Botelho <155117055+thiago2898@users.noreply.github.com>
2024-09-15 20:53:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const playerName = prompt('Olá, cidadão(ã), como se chama?')
+let startMenu = prompt(`Seja bem-vindo(a), ${playerName}!
+Escolha uma das opções abaixo para começar.
+
+1- Jogar
+2- Sair
+    `)
+
+const secretNumber = Math.floor(Math.random() * 11)
+let attempt
+let remainingAttempts = 5
+
+
+switch(startMenu) {
+    case '1':
+        do {
+    attempt = prompt('Adivinhe o número secreto (entre 1 e 10):')
+
+    if (attempt == secretNumber) {
+    alert(`Parabéns ${playerName}, você adivinhou o número secreto!`)
+    break
+    } else if (attempt > secretNumber) {
+    alert('O número secreto é menor.')
+    } else {
+        alert('O número secreto é maior.')
+    }
+
+
+    for (remainingAttempts; remainingAttempts--;) {
+        alert(`Você tem ${remainingAttempts} tentativas restantes.`)
+        break
+    }
+
+    } while (remainingAttempts > 0)
+
+
+
+    while (remainingAttempts == 0) {
+        alert(`Você perdeu, o número secreto era ${secretNumber}.`)
+        break
+    }
+
+        break
+
+    case '2':
+        alert('Ok, espero você de volta.')
+        break
+    default:
+        alert('Opção inválida/inexistente.')
+}
