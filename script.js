164eb1bebb4c3956d5ef0c26cb8df40dbb016d59
Thiago Botelho <155117055+thiago2898@users.noreply.github.com>
2024-09-19 08:49:55 -0300

    script.js

--- script.js
+++ script.js
@@ -14,33 +14,33 @@ let remainingAttempts = 5
 switch(startMenu) {
     case '1':
         do {
-    attempt = prompt('Adivinhe o número secreto (entre 1 e 10):')
+            attempt = prompt('Adivinhe o número secreto (entre 1 e 10):')
 
-    if (attempt == secretNumber) {
-    alert(`Parabéns ${playerName}, você adivinhou o número secreto!`)
-    break
-    } else if (attempt > secretNumber) {
-    alert('O número secreto é menor.')
-    } else {
-        alert('O número secreto é maior.')
-    }
+            if (attempt == secretNumber) {
+                alert(`Parabéns ${playerName}, você adivinhou o número secreto!`)
+                break
+            } else if (attempt > secretNumber) {
+                alert('O número secreto é menor.')
+            } else {
+                alert('O número secreto é maior.')
+            }
 
 
-    for (remainingAttempts; remainingAttempts--;) {
-        alert(`Você tem ${remainingAttempts} tentativas restantes.`)
+        for (remainingAttempts; remainingAttempts--;) {
+            alert(`Você tem ${remainingAttempts} tentativas restantes.`)
+            break
+        }
+
+        } while (remainingAttempts > 0)
+
+
+
+        while (remainingAttempts == 0) {
+            alert(`Você perdeu, o número secreto era ${secretNumber}.`)
+            break
+        }
+
         break
-    }
-
-    } while (remainingAttempts > 0)
-
-
-
-    while (remainingAttempts == 0) {
-        alert(`Você perdeu, o número secreto era ${secretNumber}.`)
-        break
-    }
-
-        break // Refere-se ao case 1.
 
     case '2':
         alert('Ok, espero você de volta.')
